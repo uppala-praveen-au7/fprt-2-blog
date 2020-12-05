f5bca6c8bd18ef671bcefea44d3c7b0746cf1254
wiz-coder <procodevnin@gmail.com>
2020-12-05 19:56:26 +0530

    FPRT BLOG: created server

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import morgan from "morgan"
+import "dotenv/config"
+import mongoose from "mongoose"
+import cors from "cors"
+
+const app = express()
+
+
+// middlewares
+app.use(cors())
+app.use(morgan("combined"))
+
+// database connection
+mongoose.connect(process.env.DB_CONNECTION,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+.then(()=>console.log('Database connected'))
+.catch(err=>console.log(err))
+
+app.use('/',(req,res)=>{
+    res.send('Welcome to Blog backend')
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>console.log(`server listening to ${PORT}`))
